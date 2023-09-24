1aeb73d48b4e2bee8ed8fd56dfdfdbe2278fc544
billlims <davidlinxf@gmail.com>
2023-09-24 19:18:34 +1000

    adding close, clear show and strech picture features

--- EveentTesting/Form1.cs
+++ EveentTesting/Form1.cs
@@ -20,6 +20,7 @@ namespace EveentTesting
 
         private void showButton_Click(object sender, EventArgs e)
         {
+            Console.WriteLine(e);
             // Show the Open File dialog. If the user clicks OK, load the
             // picture that the user chose.
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
